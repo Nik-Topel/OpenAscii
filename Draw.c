352be3928f29cb5b83dc0f0ccbee81ef3fca9e82
Nik-Topel <60180879+Nik-Topel@users.noreply.github.com>
2021-11-03 18:36:40 +0100

    sliming of code

--- Draw.c
+++ Draw.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 #include<math.h>
 #include"draw.h"
-
 int c;
 int r;
 char f[30000];
-
 int cords_t_pixels(int p, float i){return i / (1.0 / (p-0.5));}
-
 void draw_background(char i){
-	int x;
-	for(x = 0; x < (r * c); x = x + 1){
+	for(int x = 0; x < (r * c); x++){
 		f[x] = i;
 	}
 }
@@ -21,9 +17,8 @@ void draw_pixel_real(char i,int x, int y){
 	draw_pixel(i,cords_t_pixels(c, x),cords_t_pixels(r, y));
 }
 void draw_rect(char i,int x1, int y1,int x2, int y2){
-	int x,y;
-	for(x = x1; x < x2; x = x + 1){
-		for(y = y1; y < y2; y = y + 1){
+	for(int x = x1; x < x2; x++){
+		for(int y = y1; y < y2; y++){
 			draw_pixel(i,x,y);
 		}
 	}
@@ -32,33 +27,31 @@ void draw_rect_real(char i,float x1, float y1,float x2, float y2){
 	draw_rect(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2));
 }
 void draw_line(char i,float x1, float y1,float x2, float y2){
-	int x;
-	int y;
 	if(y2<y1){
-		float tmp1= y2;
+		float tmp= y2;
 		y2 = y1;
-		y1= tmp1;
-		float tmp2= x2;
+		y1= tmp;
+		tmp= x2;
 		x2 = x1;
-		x1 = tmp2;
+		x1 = tmp;
 	}
 	if(x2<x1){
-		float tmp1= x2;
+		float tmp= x2;
 		x2 = x1;
-		x1 = tmp1;
-		float tmp2= y2;
+		x1 = tmp;
+		tmp= y2;
 		y2 = y1;
-		y1 = tmp2;
+		y1 = tmp;
 	}
 	if((y2-y1)<=(x2-x1)){
-		for(x= x1;x<x2;x=x+1){
-			y = (((x-x1)/(x2-x1))*(y2-y1))+y1+0.5;
+		for(int x= x1;x<x2;x++){
+			int y = (((x-x1)/(x2-x1))*(y2-y1))+y1+0.5;
 			draw_pixel(i,x,y);
 		}
 	}
 	if((x2-x1)<(y2-y1)){
-		for(x= y1;x<y2;x=x+1){
-			y = (((y-y1)/(y2-y1))*(x2-x1))+x1+0.5;
+		for(int x= y1;x<y2;x++){
+			int y = (((y-y1)/(y2-y1))*(x2-x1))+x1+0.5;
 			draw_pixel(i,y,x);
 		}
 	}
@@ -75,8 +68,7 @@ void draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
     int pointNum = sqrt(((x3-x2)*(x3-x2))+((y3-y2)*(y3-y2)));
     float interval_X = diff_X / (pointNum + 1);
     float interval_Y = diff_Y / (pointNum + 1);
-	int x;
-    for(x = 1; x <= pointNum; x=x+1)
+    for(int x = 1; x <= pointNum; x++)
     {
 		draw_line(i,x1,y1,x2+interval_X*x,y2 + interval_Y*x);
     }
@@ -84,27 +76,15 @@ void draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 void draw_tri_real(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_tri(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2),cords_t_pixels(c, x3),cords_t_pixels(r, y3));
 }
-//void draw_polygon(char i,float cords[20]){
-	//int x;
-	//for(x=0;x<1;x=x+1){
-		
-	//}
-//}
 void draw_frame(){
 	draw_background(' ');
-	
 	draw_pixel_real('#',0,0);
-	
-	//draw_rect_real('#',0.1,0.1,0.3,0.3);
-	
-	//draw_line_real('#',0.1,0.5,0.6,0.95);
-	
-	draw_tri_real('#',0,0.5,1,1,0.25,0);
-	
+	draw_rect_real('#',0.1,0.1,0.3,0.3);
+	draw_line_real('#',0.1,0.5,0.6,0.95);
+	draw_tri_real('#',0.25,0.4,0.5,0.75,0.75,0.1);
 	system("cls");
 	printf(f);
 }
-
 int main(){
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
