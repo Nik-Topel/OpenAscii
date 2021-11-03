5605dae935bd02d9b77fd271f73f816044ee49b5
Nik-Topel <60180879+Nik-Topel@users.noreply.github.com>
2021-11-03 22:28:17 +0100

    optimisation

--- Draw.c
+++ Draw.c
@@ -1,30 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 #include"draw.h"
-int c;
-int r;
+int c,r;
 char f[30000];
-int cords_t_pixels(int p, float i){return i / (1.0 / (p-0.5));}
+int real_x(float i){return (i / (1.0 / (c-0.5)))+(c/2);}
+int real_y(float i){return (i / (1.0 / (r-0.5)))+(r/2);}
 void draw_background(char i){
-	for(int x = 0; x < (r * c); x++){
-		f[x] = i;
-	}
+	for(int x = 0; x < (r * c); x++){f[x] = i;}
 }
 void draw_pixel(char i,int x, int y){
 	f[(((r-(y +1))*c)+x)] = i;
 }
-void draw_pixel_real(char i,int x, int y){
-	draw_pixel(i,cords_t_pixels(c, x),cords_t_pixels(r, y));
-}
 void draw_rect(char i,int x1, int y1,int x2, int y2){
-	for(int x = x1; x < x2; x++){
-		for(int y = y1; y < y2; y++){
-			draw_pixel(i,x,y);
-		}
-	}
-}
-void draw_rect_real(char i,float x1, float y1,float x2, float y2){
-	draw_rect(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2));
+	for(int x = x1; x < x2; x++){for(int y = y1; y < y2; y++){draw_pixel(i,x,y);}}
 }
 void draw_line(char i,float x1, float y1,float x2, float y2){
 	if(y2<y1){
@@ -51,37 +39,28 @@ void draw_line(char i,float x1, float y1,float x2, float y2){
 	}
 	if((x2-x1)<(y2-y1)){
 		for(int x= y1;x<y2;x++){
-			int y = (((y-y1)/(y2-y1))*(x2-x1))+x1+0.5;
+			int y = (((x-y1)/(y2-y1))*(x2-x1))+x1+0.5;
 			draw_pixel(i,y,x);
 		}
 	}
-}
-void draw_line_real(char i,float x1, float y1,float x2, float y2){
-	draw_line(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2));
 }
 void draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_line(i,x1,y1,x2,y2);
 	draw_line(i,x2,y2,x3,y3);
 	draw_line(i,x3,y3,x1,y1);
-	float diff_X = x3 - x2;
-	float diff_Y = y3 - y2;
-	int pointNum = sqrt(((x3-x2)*(x3-x2))+((y3-y2)*(y3-y2)));
-	float interval_X = diff_X / (pointNum + 1);
-	float interval_Y = diff_Y / (pointNum + 1);
-	for(int x = 1; x <= pointNum; x++)
-	{
-		draw_line(i,x1,y1,x2+interval_X*x,y2 + interval_Y*x);
-	}
-}
-void draw_tri_real(char i,float x1, float y1,float x2, float y2,float x3, float y3){
-	draw_tri(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2),cords_t_pixels(c, x3),cords_t_pixels(r, y3));
+	float diff_x = x3 - x2;
+	float diff_y = y3 - y2;
+	int points = sqrt(((x3-x2)*(x3-x2))+((y3-y2)*(y3-y2)));
+	float interval_x = diff_x / (points + 1);
+	float interval_y = diff_y / (points + 1);
+	for(int x = 1; x <= points; x++){draw_line(i,x1,y1,x2+interval_x*x,y2 + interval_y*x);}
 }
 void draw_frame(){
 	draw_background(' ');
-	draw_pixel_real('#',0,0);
-	draw_rect_real('#',0.1,0.1,0.3,0.3);
-	draw_line_real('#',0.1,0.5,0.6,0.95);
-	draw_tri_real('#',0.25,0.4,0.5,0.75,0.75,0.1);
+	draw_pixel('#',real_x(-0.5),real_y(-0.5));
+	draw_pixel('#',real_x(-0.5),real_y(0.5));
+	draw_pixel('#',real_x(0.5),real_y(-0.5));
+	draw_pixel('#',real_x(0.5),real_y(0.5));
 	system("cls");
 	printf(f);
 }
@@ -92,9 +71,7 @@ int main(){
 	r = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 	while(1){
 		draw_frame();
-		if(kbhit()){
-			return 0;
-		}
+		if(kbhit()){return 0;}
 		Sleep(100);
 	}
 }
