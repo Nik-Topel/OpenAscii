bee9be71f62d222b48514e6b75ddb469fb9ae5f0
Nik-Topel <60180879+Nik-Topel@users.noreply.github.com>
2021-11-03 15:10:51 +0100

    remove prototypes

--- Draw.c
+++ Draw.c
@@ -11,76 +11,33 @@ int c;
 int r;
 char f[30000];
 
-int draw_rect_real(char,float,float,float,float);
-int draw_line_real(char,float,float,float,float);
-int draw_tri_real(char,float,float,float,float,float,float);
-
-int main(){
-	bool running = true;
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	c = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	r = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-	while(running){
-		draw_frame();
-		if(kbhit()){
-			running = false;
-		}
-		Sleep(100);
-	}
-	return 0;
-}
-
-int draw_frame(){
-	draw_background(' ');
-	
-	draw_pixel_real('#',0,0);
-	
-	draw_rect_real('#',0.1,0.1,0.3,0.3);
-	
-	draw_line_real('#',0.1,0.5,0.6,0.95);
-	
-	draw_tri_real('#',0.25,0.4,0.5,0.75,0.75,0.1);
-	
-	
-	
-	system("cls");
-	printf(f);
-	
-	return 0;
-}
 int cords_t_pixels(int p, float i){
 	return i / (1.0 / (p-0.5));
 }
-int draw_background(char i){
+void draw_background(char i){
 	int x;
 	for(x = 0; x < (r * c); x = x + 1){
 		f[x] = i;
 	}
-	return 0;
 }
-int draw_pixel(char i,int x, int y){
+void draw_pixel(char i,int x, int y){
 	f[(((r-(y +1))*c)+x)] = i;
-	return 0;
 }
-int draw_pixel_real(char i,int x, int y){
+void draw_pixel_real(char i,int x, int y){
 	draw_pixel(i,cords_t_pixels(c, x),cords_t_pixels(r, y));
-	return 0;
 }
-int draw_rect(char i,int x1, int y1,int x2, int y2){
+void draw_rect(char i,int x1, int y1,int x2, int y2){
 	int x,y;
 	for(x = x1; x < x2; x = x + 1){
 		for(y = y1; y < y2; y = y + 1){
 			draw_pixel(i,x,y);
 		}
 	}
-	return 0;
 }
-int draw_rect_real(char i,float x1, float y1,float x2, float y2){
+void draw_rect_real(char i,float x1, float y1,float x2, float y2){
 	draw_rect(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2));
-	return 0;
 }
-int draw_line(char i,float x1, float y1,float x2, float y2){
+void draw_line(char i,float x1, float y1,float x2, float y2){
 	int x;
 	int y;
 	bool rev_x;
@@ -113,26 +70,49 @@ int draw_line(char i,float x1, float y1,float x2, float y2){
 			draw_pixel(i,y,x);
 		}
 	}
-	return 0;
 }
-int draw_line_real(char i,float x1, float y1,float x2, float y2){
+void draw_line_real(char i,float x1, float y1,float x2, float y2){
 	draw_line(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2));
-	return 0;
 }
-int draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
+void draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_line(i,x1,y1,x2,y2);
 	draw_line(i,x2,y2,x3,y3);
 	draw_line(i,x3,y3,x1,y1);
-	return 0;
 }
-int draw_tri_real(char i,float x1, float y1,float x2, float y2,float x3, float y3){
+void draw_tri_real(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_tri(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2),cords_t_pixels(c, x3),cords_t_pixels(r, y3));
-	return 0;
 }
-int draw_polygon(char i,float cords[20]){
+void draw_polygon(char i,float cords[20]){
 	int x;
 	for(x=0;x<1;x=x+1){
 		
 	}
-	return 0;
+}
+void draw_frame(){
+	draw_background(' ');
+	
+	draw_pixel_real('#',0,0);
+	
+	draw_rect_real('#',0.1,0.1,0.3,0.3);
+	
+	draw_line_real('#',0.1,0.5,0.6,0.95);
+	
+	draw_tri_real('#',0.25,0.4,0.5,0.75,0.75,0.1);
+	
+	system("cls");
+	printf(f);
+}
+
+int main(){
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	c = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+	r = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+	while(1){
+		draw_frame();
+		if(kbhit()){
+			return 0;
+		}
+		Sleep(100);
+	}
 }
