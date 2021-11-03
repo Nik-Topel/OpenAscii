71e7a353e395c6da96d79e5cbb4248472e20f825
Nik-Topel <60180879+Nik-Topel@users.noreply.github.com>
2021-11-03 17:16:07 +0100

    triangle fill

--- Draw.c
+++ Draw.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 #include"draw.h"
 
 int c;
 int r;
 char f[30000];
 
-int cords_t_pixels(int p, float i){
-	return i / (1.0 / (p-0.5));
-}
+int cords_t_pixels(int p, float i){return i / (1.0 / (p-0.5));}
+
 void draw_background(char i){
 	int x;
 	for(x = 0; x < (r * c); x = x + 1){
@@ -70,28 +70,38 @@ void draw_tri(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_line(i,x1,y1,x2,y2);
 	draw_line(i,x2,y2,x3,y3);
 	draw_line(i,x3,y3,x1,y1);
+	float diff_X = x3 - x2;
+    float diff_Y = y3 - y2;
+    int pointNum = sqrt(((x3-x2)*(x3-x2))+((y3-y2)*(y3-y2)));
+    float interval_X = diff_X / (pointNum + 1);
+    float interval_Y = diff_Y / (pointNum + 1);
+	int x;
+    for(x = 1; x <= pointNum; x=x+1)
+    {
+		draw_line(i,x1,y1,x2+interval_X*x,y2 + interval_Y*x);
+    }
 }
 void draw_tri_real(char i,float x1, float y1,float x2, float y2,float x3, float y3){
 	draw_tri(i,cords_t_pixels(c, x1),cords_t_pixels(r, y1),cords_t_pixels(c, x2),cords_t_pixels(r, y2),cords_t_pixels(c, x3),cords_t_pixels(r, y3));
 }
-void draw_polygon(char i,float cords[20]){
-	int x;
-	for(x=0;x<1;x=x+1){
+//void draw_polygon(char i,float cords[20]){
+	//int x;
+	//for(x=0;x<1;x=x+1){
 		
-	}
-}
+	//}
+//}
 void draw_frame(){
 	draw_background(' ');
 	
 	draw_pixel_real('#',0,0);
 	
-	draw_rect_real('#',0.1,0.1,0.3,0.3);
+	//draw_rect_real('#',0.1,0.1,0.3,0.3);
 	
-	draw_line_real('#',0.1,0.5,0.6,0.95);
+	//draw_line_real('#',0.1,0.5,0.6,0.95);
 	
-	draw_tri_real('#',0.25,0.4,0.5,0.75,0.75,0.1);
+	draw_tri_real('#',0,0.5,1,1,0.25,0);
 	
-	//system("cls");
+	system("cls");
 	printf(f);
 }
 
