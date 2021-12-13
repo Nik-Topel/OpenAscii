42af6b4eac24039f36530e82ce6fcbab55c0ab76
Nik <60180879+Nik-Topel@users.noreply.github.com>
2021-12-13 15:07:55 +0100

    Update renderer.c

--- renderer.c
+++ renderer.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<math.h>
 #include<string.h>
@@ -7,27 +8,18 @@
 #elif defined(_WIN32) || defined(WIN32)
 #include<windows.h>
 #endif
-//double perspective;
+double perspective;
 char buffer[30000];
 int columns,rows;
-int real(float i,int p){
-        return(i/(1.0/(p-0.5)))+(p/2);}
+int real(float input,int length){
+        return(input/(1.0/(length-0.5)))+(length/2);}
 int* perspective(float x,float y,float z){
         int o[3]={x*Z0/(Z0+z),y*Z0/(Z0+z),z};
         return o;}
 float argument_parser(int start_pos,char arg[50]){
         char output[50]=arg;
         strncpy(output,start_pos,start_pos+5);
-        return atof(output);
-void clear(){
-        #ifdef __unix__
-        printf("\033[%d;%dH", (0), (0));
-        #elif defined(_WIN32) || defined(WIN32)
-        COORD cd;
-        cd.X=0,cd.Y=0;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),cd);
-        #endif
-}
+        return atof(output);}
 void draw_background(char i){
         for(int x=0;x<(rows*columns);x++){
                 buffer[x]=i;}}
@@ -55,25 +47,31 @@ void draw_triangle(char i,float x1,float y1,float x2,float y2,float x3,float y3)
         for(int x=1;x<=points;x++){
                 draw_line(i,x1,y1,x2+interval_x*x,y2+interval_y*x);}}
 void draw(char *args[100]){
-        if(argv = 1){
-                draw_background(args[1]);
-                for(x=1;x<(sizeof(args)-2);x++){
-                        if(args[x][1]="r"){
-                                draw_rectangle(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows));}
-                        else if(args[x][1]="t"){
-                                draw_triangle(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows),real(argument_parser(23,args[x]),columns),real(argument_parser(28,args[x]),rows));}
-                        else if(args[x][1]="l"){
-                                draw_line(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows));}
-                        else if(args[x][1]="p"){
-                                draw_pixel(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows));}}}
-        clear(),printf(buffer);}
+        draw_background(args[1]);
+        for(x=1;x<(sizeof(args)-2);x++){
+                if(args[x][1]="r"){
+                        draw_rectangle(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows));}
+                else if(args[x][1]="t"){
+                        draw_triangle(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows),real(argument_parser(23,args[x]),columns),real(argument_parser(28,args[x]),rows));}
+                else if(args[x][1]="l"){
+                        draw_line(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows),real(argument_parser(13,args[x]),columns),real(argument_parser(18,args[x]),rows));}
+                else if(args[x][1]="p"){
+                        draw_pixel(args[x][2],real(argument_parser(3,args[x]),columns),real(argument_parser(8,args[x]),rows));}}
+        #ifdef __unix__
+        printf("\033[%d;%dH", (0), (0));
+        #elif defined(_WIN32) || defined(WIN32)
+        COORD cd;
+        cd.X=0,cd.Y=0;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),cd);
+        #endif
+        printf(buffer);}
 int main(int argc,char *argv[]){
         #ifdef __unix__
         struct winsize w;
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
         rows=w.ws_row,columns=w.ws_col;
         #elif defined(_WIN32) || defined(WIN32) 
-        CONSOLE_SCREEN_BUFFER_INFO csbi;                                                                                                                                                                                                                                                                                                                                                     
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&csbi);
         columns=csbi.srWindow.Right-csbi.srWindow.Left+1,rows=csbi.srWindow.Bottom-csbi.srWindow.Top+1;
         #endif
